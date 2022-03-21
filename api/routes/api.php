ef8318beaf26bbb1f0ee0c59548f833ef55f6aa0
mustafabulut <mustafabulut@gmail.com>
2022-03-22 01:19:43 +0300

    Backend Eklendi

--- api/routes/api.php
+++ api/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::controller(ProductController::class)->group(function(){
+    Route::get('/products', 'index');
+    Route::post('/product', 'store');
+    Route::get('/product/{id}', 'show');
+    Route::put('/product/{id}', 'update');
+    Route::delete('/product/{id}', 'destroy');
 });
